fa8feeb28e3371d8a6b19bf4de5798d4102e951a
AlexPurhalo <alexpurhalo@gmail.com>
2017-06-20 14:10:55 +0300

    drinks-fetching-by-client-side

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    componentDidMount() {
+        window.fetch('api/drinks')
+            .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.log(error))
+    }
+    render() {
+        return (
+            <div className="App">
+            <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+        </div>
     );
-  }
+    }
 }
 
 export default App;
